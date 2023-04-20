4ad85f4ddba3d15ecafd7faaebcd7274e1ccbcf6
Markus Suonto <markus.suonto@withsecure.com>
2023-04-20 09:51:37 +0300

    destructuring compromises type safety

--- index.ts
+++ index.ts
@@ -49,9 +49,38 @@ console.log(fishFoodTexturePreferences.fish_soup);
 
 /**
  * This is an array with 1-3 strings in it.
+ * Or artificially, a tuple type with optional elements.
+ * (in reality tuples are arrays with fixed elements).
  *
  * Examples:
  * const myArray: OneToThreeStrings = []; -> "Source has 0 element(s) but target requires 1"
  * const myArray: OneToThreeStrings = ["foo", "foo", "foo", "foo"]; -> "Source has 4 element(s) but target allows only 3"
  */
 type OneToThreeStrings = [string, string?, string?];
+
+const myArr: OneToThreeStrings = ["foo"];
+/**
+ * Using pop on a tuple is mutilation and completely wrong.
+ * Typescript would surely like to forbid that (compile error),
+ * but can't since it can't distinguish tuples from arrays as
+ * they are in-fact arrays.
+ * :clown_face:
+ */
+myArr.pop();
+// runtime error accessing first from OneToThreeStrings
+myArr[0];
+
+/**
+ * This example messes up with the compiler.
+ * Empty array destructuring results in array element type,
+ * when in reality the value is undefined.
+ * Destructuring arrays and objects in general is not type-safe.
+ * Destructuring (unmutilated) tuples is type-safe (for example the first element of OneToThreeStrings).
+ */
+const array = ["foo"];
+array.pop();
+
+// The type of foo here is string. In reality foo is undefined.
+const [foo] = array;
+
+console.log(foo);
